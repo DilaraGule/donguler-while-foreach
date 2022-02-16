0f9f0ba1328e8b4ab5e47eef594ebb5065304ebc
DilaraGule <80154346+DilaraGule@users.noreply.github.com>
2022-02-16 18:46:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac <= sayi){
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama= " + toplam/sayi);
+
+            //foreach
+            string[] arabalar = {"hyundai", "toyota", "suzuki", "nissan"};
+
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
